769b5053eadfab754a5d53b71c918f0752360960
Elio Reyes Silva <resiel@gmail.com>
2021-01-27 06:13:19 -0600

    Creating our first project spring boot from spring initializr.

---
+++ 2-booking-restaurant-api/src/main/java/mx/qosoft/bookingrestaurantapi/BookingRestaurantApiApplication.java
@@ -0,0 +1,14 @@
+package mx.qosoft.bookingrestaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingRestaurantApiApplication.class, args);
+                System.out.println("Hello Spring Framework...");
+	}
+
+}
